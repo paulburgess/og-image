81c31632eb62b7352e90a0c194c6981498f9ca61
Paul Burgess <paul@delargedesign.com>
2019-12-31 15:58:58 +0000

    removed let

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -12,8 +12,9 @@ const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('b
 const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
 
 function getCss(theme: string, fontSize: string) {
-    let background = 'white';
-    let foreground = 'black';
+
+    // let background = 'white';
+    // let foreground = 'black';
   //  let radial = 'lightgray';
 
     if (theme === 'dark') {
